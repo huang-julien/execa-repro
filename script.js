d4e238b4946d43101ed2d28ee8f14554a5c0e729
Julien Huang <julien.h.dev@gmail.com>
2024-05-26 23:55:21 +0200

    reduce repro

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
-import express from "express"
-const app = express()
-const port = 3000
-
 process.on('SIGTERM', () => {
     console.log('SIGTERM signal received')
     process.exit()
 })
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+setTimeout(() => {
+ throw new Error('This should have been closed by index.js')
+}, 10000)
